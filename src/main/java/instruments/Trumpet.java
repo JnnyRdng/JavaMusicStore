758e8da32b309f0955d3ac9328c72a6e33138bd5
Jonny Reading <jonnyreading@gmail.com>
2020-10-02 12:46:39 +0100

    Trumpet class and tests

--- src/main/java/instruments/Trumpet.java
+++ src/main/java/instruments/Trumpet.java
@@ -1,4 +1,19 @@
 package instruments;
 
-public class Trumpet {
+public class Trumpet extends Instrument {
+
+    private int valves;
+
+    public Trumpet(String colour, String brand, int boughtPrice, int sellPrice) {
+        super("metal", colour, "brass", brand, boughtPrice, sellPrice);
+        this.valves = 3;
+    }
+
+    public int getValves() {
+        return valves;
+    }
+
+    public String play() {
+        return "parrrrp";
+    }
 }
--- src/test/java/instruments/TrumpetTest.java
+++ src/test/java/instruments/TrumpetTest.java
@@ -1,4 +1,26 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TrumpetTest {
+
+    private Trumpet trumpet;
+
+    @Before
+    public void before() {
+        trumpet = new Trumpet("gold", "Yamaha", 200, 300);
+    }
+
+    @Test
+    public void canGetNumberOfValves() {
+        assertEquals(3, trumpet.getValves());
+    }
+
+    @Test
+    public void canPlayTrumpet() {
+        assertEquals("parrrrp", trumpet.play());
+    }
 }
